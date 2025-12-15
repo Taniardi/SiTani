51db995affd3414c6521e5110809b89d77f367f3
tasyapndya <tasya.pandya@gmail.com>
2025-12-15 15:46:04 +0700

    add structure folder

--- app/src/main/java/edu/bluejack24_2/myapplication/activities/MainActivity.java
+++ app/src/main/java/edu/bluejack24_2/myapplication/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package edu.bluejack24_2.myapplication;
+package edu.bluejack24_2.myapplication.activities;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import edu.bluejack24_2.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
